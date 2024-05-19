223a3804a684a33172142ea14ffb3aae69902937
mahmoud gamal <mahmoudgamalhafez@gmail.com>
2024-05-19 23:24:16 +0300

    spring security init

---
+++ SpringSecurity/src/test/java/spring_Security/example/SpringSecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_Security.example.SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
